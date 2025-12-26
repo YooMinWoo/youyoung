03df6fb453d7b09c9ad12d37554a7c9a8bff3c3b
YooMinWoo <alsn0527@naver.com>
2025-12-26 13:05:27 +0900

    init prj

---
+++ src/main/java/com/example/youyoung/YouyoungApplication.java
@@ -0,0 +1,13 @@
+package com.example.youyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouyoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouyoungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/youyoung/YouyoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youyoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouyoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
