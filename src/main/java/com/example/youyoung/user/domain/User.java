133f4a0763a287c4510849c9df7fa7dc07272979
YooMinWoo <alsn0527@naver.com>
2025-12-26 15:09:03 +0900

    Feat : User 엔티티 생성

---
+++ src/main/java/com/example/youyoung/user/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.youyoung.user.domain;
+
+import com.example.youyoung.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
