db676dac137cb8bd398b5a9f3f093e5620d8bf0a
YooMinWoo <alsn0527@naver.com>
2025-12-26 15:09:36 +0900

    Feat : 유저, 상품 더미데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO users (id, name, created_time, modified_time) VALUES
+(1, 'test_1', now(), now());
+
+INSERT INTO product (id, name, price, quantity, created_time, modified_time) VALUES
+(1, 'product1',  (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(2, 'product2',  (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(3, 'product3',  (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(4, 'product4',  (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(5, 'product5',  (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(6, 'product6',  (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(7, 'product7',  (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(8, 'product8',  (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(9, 'product9',  (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(10,'product10', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(11,'product11', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(12,'product12', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(13,'product13', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(14,'product14', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(15,'product15', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(16,'product16', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(17,'product17', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(18,'product18', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(19,'product19', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(20,'product20', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(21,'product21', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(22,'product22', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(23,'product23', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(24,'product24', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW()),
+(25,'product25', (FLOOR(RAND() * 10) + 1) * 5000, FLOOR(RAND() * 100) + 1, NOW(), NOW());
